8eb027029e7f82669d74bcacb70e7aa4e002825b
GeorgiStavrev <georgi@insurify.com>
2021-05-31 21:48:53 +0300

    adds one more table

--- dynamodb.tf
+++ dynamodb.tf
@@ -125,3 +125,24 @@ resource "aws_dynamodb_table" "domains" {
     projection_type = "KEYS_ONLY"
   }
 }
+
+resource "aws_dynamodb_table" "loadtest_metrics" {
+  name           = "loadtest_metrics"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "LoadtestId"
+  range_key      = "MetricName"
+
+  attribute {
+    name = "LoadtestId"
+    type = "S"
+  }
+
+  attribute {
+    name = "MetricName"
+    type = "S"
+  }
+
+  // Data
+}
