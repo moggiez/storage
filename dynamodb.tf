a09617e37ae3bbad95b3f08b11f17caa25cc307f
GeorgiStavrev <georgi@insurify.com>
2021-07-18 13:26:43 +0300

    moves table to microsvc repo

--- dynamodb.tf
+++ dynamodb.tf
@@ -19,41 +19,6 @@ resource "aws_dynamodb_table" "playbooks" {
   // Playbook
 }
 
-resource "aws_dynamodb_table" "playbook_versions" {
-  name           = "playbook_versions"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 5
-  write_capacity = 5
-  hash_key       = "PlaybookId"
-  range_key      = "Version"
-
-  attribute {
-    name = "PlaybookId"
-    type = "S"
-  }
-
-  attribute {
-    name = "Version"
-    type = "S"
-  }
-
-  attribute {
-    name = "OrganisationId"
-    type = "S"
-  }
-
-  global_secondary_index {
-    name            = "OrganisationPlaybooks"
-    hash_key        = "OrganisationId"
-    range_key       = "PlaybookId"
-    write_capacity  = 5
-    read_capacity   = 5
-    projection_type = "ALL"
-  }
-
-  // Playbook
-}
-
 resource "aws_dynamodb_table" "loadtests" {
   name           = "loadtests"
   billing_mode   = "PROVISIONED"
